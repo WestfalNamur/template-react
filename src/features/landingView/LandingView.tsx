8095851128fcd7e27b9f18117e58d6ed19bca453
Stefan Crummenerl <crummenerls@gmail.com>
2022-01-24 15:49:59 -0300

    Refactor

--- src/features/landingView/LandingView.tsx
+++ src/features/landingView/LandingView.tsx
@@ -1,29 +1,6 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
-import Image from "next/image";
+import LandingViewCard from "./LandingViewCard";
 
-const LandingViewCard = (props: { className: string }): JSX.Element => {
-  const { className } = props;
-  const [hovered, setHovered] = useState(false);
-
-  return (
-    <motion.div
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      className={className ? className : "h-16 xs:h-auto xs:square"}
-      whileHover={{ scale: 1.1 }}
-    >
-      {hovered && (
-        // https://stackoverflow.com/questions/67421778/next-js-image-layout-fill-is-broken
-        <div style={{ width: "100%", height: "100%", position: "relative" }}>
-          <Image src="/dino.jpg" alt="Dino" layout="fill" objectFit="cover" />
-        </div>
-      )}
-    </motion.div>
-  );
-};
-
-const LandingView = () => {
+export default function LandingView() {
   return (
     <div className="grid place-items-center min-h-screen">
       <div className="p-4 max-w-5xl grid gap-4 xs:grid-cols-2 xs:p-8 md:grid-cols-4 lg:gap-6">
@@ -35,14 +12,38 @@ const LandingView = () => {
           energy. One might wonder how such a big heart fits in such a small
           person?
         </p>
-        <LandingViewCard className="h-16 bg-blue-500 xs:h-auto xs:square" />
-        <LandingViewCard className="h-16 bg-blue-500 xs:h-auto xs:square" />
-        <LandingViewCard className="h-16 bg-pink-500 xs:h-auto xs:square" />
-        <LandingViewCard className="h-16 bg-blue-500 xs:h-auto xs:square md:col-start-2" />
-        <LandingViewCard className="h-16 bg-pink-500 xs:h-auto xs:square" />
-        <LandingViewCard className="h-16 bg-blue-500 xs:h-auto xs:square" />
-        <LandingViewCard className="h-16 bg-blue-500 xs:h-auto xs:square" />
-        <LandingViewCard className="h-16 bg-pink-500 xs:h-auto xs:square" />
+        <LandingViewCard
+          imgSrc="/dino.jpg"
+          className="h-16 bg-blue-500 xs:h-auto xs:square"
+        />
+        <LandingViewCard
+          imgSrc="/dino.jpg"
+          className="h-16 bg-blue-500 xs:h-auto xs:square"
+        />
+        <LandingViewCard
+          imgSrc="/dino.jpg"
+          className="h-16 bg-pink-500 xs:h-auto xs:square"
+        />
+        <LandingViewCard
+          imgSrc="/dino.jpg"
+          className="h-16 bg-blue-500 xs:h-auto xs:square md:col-start-2"
+        />
+        <LandingViewCard
+          imgSrc="/dino.jpg"
+          className="h-16 bg-pink-500 xs:h-auto xs:square"
+        />
+        <LandingViewCard
+          imgSrc="/dino.jpg"
+          className="h-16 bg-blue-500 xs:h-auto xs:square"
+        />
+        <LandingViewCard
+          imgSrc="/dino.jpg"
+          className="h-16 bg-blue-500 xs:h-auto xs:square"
+        />
+        <LandingViewCard
+          imgSrc="/dino.jpg"
+          className="h-16 bg-pink-500 xs:h-auto xs:square"
+        />
         <p className="self-center md:text-lg md:col-span-2 md:text-center md:px-4">
           But everything has a dark side... No chocolate is save from her!
           Nowhere!
@@ -50,6 +51,4 @@ const LandingView = () => {
       </div>
     </div>
   );
-};
-
-export default LandingView;
+}
