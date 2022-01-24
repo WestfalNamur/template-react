778546c70ce8cb03e674f32b8c68704114fd8e9f
Stefan Crummenerl <crummenerls@gmail.com>
2022-01-24 17:09:09 -0300

    Add Todo list ... actually we get data dummy data but we have a custom type guard.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,27 @@
+import Link from "next/link";
+
 function NavBar() {
   return (
     <ul className="flex border-b">
       <li className="-mb-px mr-1">
-        <a
-          className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
-          href="#"
-        >
-          Home
-        </a>
+        <Link href="/">
+          <a
+            className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
+            href="#"
+          >
+            Home
+          </a>
+        </Link>
       </li>
       <li className="mr-1">
-        <a
-          className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
-          href="#"
-        >
-          Todos
-        </a>
-      </li>
-      <li className="mr-1">
-        <a
-          className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
-          href="#"
-        >
-          Travel
-        </a>
+        <Link href="/Todos">
+          <a
+            className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
+            href="#"
+          >
+            Todos
+          </a>
+        </Link>
       </li>
     </ul>
   );
---
+++ src/pages/Todos.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+import TodoList from "../features/todos/TodoList";
+
+const Todos: NextPage = () => {
+  return <TodoList />;
+};
+
+export default Todos;
