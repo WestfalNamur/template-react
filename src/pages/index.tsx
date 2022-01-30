de69fbf779c9a3d0855feac593ff44dbb49c5784
Stefan Crummenerl <crummenerls@gmail.com>
2022-01-30 13:20:56 +0100

    Cleanup

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import LandingView from "../features/landingView/LandingView";
-
-const IndexPage: NextPage = () => {
-  return <LandingView />;
-};
-
-export default IndexPage;
